4d26c57aafb4610cf1f440433468eb5e56141c9e
Carlos Torres <carlostxm@gmail.com>
2021-11-10 09:51:39 +0100

    Adding actions pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { NavBar } from "./components";
+import { NAVBAR_LINKS } from "./conts";
 
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
       <header className="sticky top-0 z-40 flex flex-none w-full mx-auto bg-white lg:z-50 max-w-8xl">
-        <NavBar />
+        <NavBar links={NAVBAR_LINKS} />
       </header>
       <main>
         <div className="App-main">
